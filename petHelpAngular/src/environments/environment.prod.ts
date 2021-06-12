1126958491da00218a9ce2e2694cb4b498016f47
mmasso <mmasso@cifpfbmoll.eu>
2021-06-13 01:38:48 +0200

    feat: :sparkles: Add firebase configuration

--- petHelpAngular/src/environments/environment.prod.ts
+++ petHelpAngular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBtxeEndVPtSWrgjB6iaz5bTvtoULlpBfU",
+    authDomain: "pethelp-4495a.firebaseapp.com",
+    databaseURL: "https://pethelp-4495a.firebaseio.com",
+    projectId: "pethelp-4495a",
+    storageBucket: "pethelp-4495a.appspot.com",
+    messagingSenderId: "951175074151",
+    appId: "1:951175074151:web:ff05548940808b46789fc2",
+    measurementId: "G-X2WF86ZNGG"
+  }
 };
--- petHelpAngular/src/environments/environment.ts
+++ petHelpAngular/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBtxeEndVPtSWrgjB6iaz5bTvtoULlpBfU",
+    authDomain: "pethelp-4495a.firebaseapp.com",
+    databaseURL: "https://pethelp-4495a.firebaseio.com",
+    projectId: "pethelp-4495a",
+    storageBucket: "pethelp-4495a.appspot.com",
+    messagingSenderId: "951175074151",
+    appId: "1:951175074151:web:ff05548940808b46789fc2",
+    measurementId: "G-X2WF86ZNGG"
+  }
 };
 
 /*
