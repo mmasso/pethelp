f08acd24a572c2ff298dcca74ef0bda31b7bc77d
mmasso <mmasso@cifpfbmoll.eu>
2021-06-17 13:27:36 +0200

    feat: :bug: routing improvement

--- petHelpAngular/src/app/app-routing.module.ts
+++ petHelpAngular/src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertsComponent } from './views/alerts/alerts.component';
+import { CreatePostComponent } from './views/create-post/create-post.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { AuthGuard } from "./auth.guard";
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {
     path: '', redirectTo: 'home', pathMatch: 'full'
-  }
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'alerts', component: AlertsComponent
+  },
+  {
+    path: 'alerts/createAlert', component: CreatePostComponent, canActivate: [AuthGuard]
+  },
+  { 
+    path: 'sign-in', component: SignInComponent 
+  },
+  { 
+    path: 'sign-up', component: SignUpComponent 
+  },
+  {
+     path: 'forgot-password', component: ForgotPasswordComponent
+  },
+  {
+     path: 'email-verification', component: VerifyEmailComponent 
+  },
+  {
+    path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]
+ }
 ];
 
 @NgModule({
@@ -12,3 +46,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
