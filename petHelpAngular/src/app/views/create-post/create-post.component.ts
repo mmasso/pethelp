597e7336487147a54515a6af02adba29f88e52c8
mmasso <mmasso@cifpfbmoll.eu>
2021-06-18 20:09:59 +0200

    feat: :sparkles: Create-post. to create a advise of a pet and publish it to database

--- petHelpAngular/src/app/views/create-post/create-post.component.ts
+++ petHelpAngular/src/app/views/create-post/create-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/service/auth/auth.service';
+import { FirestoreService } from 'src/app/service/firestore/firestore.service';
+import { PetAdvise } from '../../models/pet-advise';
 
 @Component({
   selector: 'app-create-post',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  petAdvise = {} as PetAdvise
+
+  petAdviseForm = new FormGroup({
+    animalName: new FormControl(''),
+    age: new FormControl(''),
+    type: new FormControl(''),
+    breed: new FormControl(''),
+    color: new FormControl(''),
+    contact: new FormControl(''),
+    description: new FormControl(''),
+    gender: new FormControl(''),
+    image: new FormControl(''),
+    location: new FormControl(''),
+    size: new FormControl('')
+  });
+
+  constructor(public ngAuthService: AuthService, public firestoreService: FirestoreService,  private formBuilder: FormBuilder) { 
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.petAdvise.name = this.petAdviseForm.get('animalName')?.value
+    this.petAdvise.age = this.petAdviseForm.get('age')?.value
+    this.petAdvise.animalType = this.petAdviseForm.get('type')?.value
+    this.petAdvise.breed = this.petAdviseForm.get('breed')?.value
+    this.petAdvise.color = this.petAdviseForm.get('color')?.value
+    this.petAdvise.contact = this.petAdviseForm.get('contact')?.value
+    this.petAdvise.description = this.petAdviseForm.get('description')?.value
+    this.petAdvise.gender = this.petAdviseForm.get('gender')?.value
+    this.petAdvise.image = this.petAdviseForm.get('image')?.value
+    this.petAdvise.location = this.petAdviseForm.get('location')?.value
+    this.petAdvise.size = this.petAdviseForm.get('size')?.value
+    this.petAdvise.owner = JSON.parse(localStorage.getItem('user') || '{}').email
+    console.warn(this.petAdviseForm.value)
+    this.firestoreService.createPetAdvise(this.petAdvise)
+    this.petAdviseForm.reset();
   }
 
 }
